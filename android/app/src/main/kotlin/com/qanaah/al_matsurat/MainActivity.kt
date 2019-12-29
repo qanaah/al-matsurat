c65d155eda374402a384504d813ddff61d1f9274
hamzah <hamzah@alamat.com>
2019-12-29 22:52:37 +0700

    :tada: init

---
+++ android/app/src/main/kotlin/com/qanaah/al_matsurat/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.qanaah.al_matsurat
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
